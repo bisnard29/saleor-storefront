92f2ef2f1a110f1c218f33fd7941a9a68b5c20af
bisnard29 <58581090+bisnard29@users.noreply.github.com>
2020-02-15 20:25:56 +0100

    Update Page.tsx

--- src/views/Home/Page.tsx
+++ src/views/Home/Page.tsx
@@ -43,12 +43,12 @@ const Page: React.FC<{
         <div className="home-page__hero-text">
           <div>
             <span className="home-page__hero__title">
-              <h1>Final reduction</h1>
+              <h1>CDLG</h1>
             </span>
           </div>
           <div>
             <span className="home-page__hero__title">
-              <h1>Up to 70% off sale</h1>
+              <h1> <3 <3 </h1>
             </span>
           </div>
         </div>
